644e797f00aef3657bb170533c51913f05b87215
Forrest-Arnold <forkalarn@gmail.com>
2025-08-22 01:11:27 -0600

    Pause

---
+++ Slam Again/View/Components/JankyCharacter/JankyPlayer.swift
@@ -0,0 +1,18 @@
+//
+//  JankyPlayer.swift
+//  Slam Again
+//
+//  Created by Leo Arnold on 8/22/25.
+//
+
+import SwiftUI
+
+struct JankyPlayer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JankyPlayer()
+}
